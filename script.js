7361afb03b767194dcb1baac0f0487b073bafcd4
schaldach <gabimorgado0311@gmail.com>
2023-02-17 23:12:04 -0300

    desenhando os caminhos

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const playButton = document.querySelector('[name=play]')
+const restartButton = document.querySelector('[name=restart]')
 const detectorConfig = {
     modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
     enableTracking: true,
@@ -6,6 +7,7 @@ const detectorConfig = {
 };
 const video = document.querySelector('video')
 const canvas = document.querySelector('canvas')
+restartButton.onclick = () => { location.reload() }
 playButton.onclick = async () => {
     try {
         if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
@@ -32,18 +34,18 @@ playButton.onclick = async () => {
     }
 
 }
-let drawn = true
 async function predictWebcam() {
-    if(drawn){
-        drawn = false
-        const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
-        await detector.estimatePoses(video, {}).then(poses => {
-            drawResults(canvas, poses)
-        })
-        window.requestAnimationFrame(predictWebcam);
-    }
-    
+    const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+    await detector.estimatePoses(video, {}).then(poses => {
+        drawResults(canvas, poses)
+    })
+    window.requestAnimationFrame(predictWebcam);
 }
+
+const facePath = [4, 2, 0, 1, 3]
+const upperBodyPath = [10, 8, 6, 5, 7, 9]
+const bodyPath = [6, 5, 11, 12, 6]
+const lowerBodyPath = [16, 14, 12, 11, 13, 15]
 
 
 function drawResults(canvas, poses) {
@@ -56,14 +58,62 @@ function drawResults(canvas, poses) {
     })
 }
 function drawKeypoints(keypoints, ctx) {
-    for (let i = 0; i < keypoints.length; i++) {
-        const keypoint = keypoints[i];
-        console.log(keypoint)
-        const { x, y, score } = keypoint
-        if(score>0.2){
-            ctx.fillRect(x, y, 5, 5);
+    ctx.beginPath()
+    ctx.lineWidth = "5"
+    ctx.strokeStyle = "red"
+    for (i = 0; i < facePath.length; i++) {
+        let rightKeyPoint = keypoints[facePath[i]]
+        if (rightKeyPoint.score > 0.2) {
+            if (i == 0) {
+                ctx.moveTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+            else {
+                ctx.lineTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
         }
-        
     }
-    drawn = true
+    ctx.stroke()
+    ctx.closePath()
+    ctx.beginPath()
+    for (i = 0; i < upperBodyPath.length; i++) {
+        let rightKeyPoint = keypoints[upperBodyPath[i]]
+        if (rightKeyPoint.score > 0.2) {
+            if (i == 0) {
+                ctx.moveTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+            else {
+                ctx.lineTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+        }
+    }
+    ctx.stroke()
+    ctx.closePath()
+    ctx.beginPath()
+    for (i = 0; i < bodyPath.length; i++) {
+        let rightKeyPoint = keypoints[bodyPath[i]]
+        if (rightKeyPoint.score > 0.2) {
+            if (i == 0) {
+                ctx.moveTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+            else {
+                ctx.lineTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+        }
+    }
+    ctx.stroke()
+    ctx.closePath()
+    ctx.beginPath()
+    for (i = 0; i < lowerBodyPath.length; i++) {
+        let rightKeyPoint = keypoints[lowerBodyPath[i]]
+        if (rightKeyPoint.score > 0.2) {
+            if (i == 0) {
+                ctx.moveTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+            else {
+                ctx.lineTo(rightKeyPoint.x, rightKeyPoint.y)
+            }
+        }
+    }
+    ctx.stroke()
+    ctx.closePath()
 }
